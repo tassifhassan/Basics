9a846d667da19651104f06a739e53344407f7b6a
Tassif Hassan <tassifhassan@Tassifs-MacBook-Air.local>
2022-09-13 22:34:03 +0530

    Data Types

---
+++ dataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char grade = 'A';
+    string phrase = "Giraffe Academy";
+    int age = 50;
+    float pi;
+    double gpa = 2.3;
+    bool isMale =true;
+    cout << grade << phrase << age << gpa << isMale << endl;
+    return 0;
+
+}
