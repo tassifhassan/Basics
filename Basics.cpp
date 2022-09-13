2318a7fedacb7adcc3d2ecfdf29ef23f957e5632
Tassif Hassan <tassifhassan@Tassifs-MacBook-Air.local>
2022-09-13 22:03:08 +0530

    Basics

---
+++ Basics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string characterName = "Tassif";
+    int characterAge;
+    characterAge = 23;
+    cout << "There was once a man named " <<characterName <<endl;
+    cout << "He was " << characterAge << " years old " << endl;
+    characterName = "Mike";
+    cout << "He liked the name " <<characterName <<endl;
+    cout << "but didnt like being " << characterAge <<endl;
+    return 0;
+}
