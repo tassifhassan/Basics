8fb1f8bb3a06b76bc9c1f49ae4c9c94d8e511876
Tassif Hassan <tassifhassan@Tassifs-MacBook-Air.local>
2022-09-16 00:31:57 +0530

    Calculator

---
+++ calculatorPro.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int num1, num2;
+char op;
+cout << "Enter First Number: ";
+cin >> num1;
+cout << "Enter operator: ";
+cin >> op;
+cout << "Enter Second Number: ";
+cin >> num2;
+int result;
+if (op == '+'){
+    result = num1 + num2;
+
+}else if (op == '-'){
+    result = num1 - num2;
+
+}else if (op == '*'){
+    result = num1 * num2;
+
+}else if (op == '/'){
+    result = num1 / num2;
+    
+}else{
+    cout << "Invalid Operator";
+
+}
+cout << result;
+
+return 0;
+}
